494a930a5c199357bf91fb438efd15ee1b947e11
Tupo Nkhoma <tlznkhoma@gmail.com>
2023-04-13 15:33:50 +1000

    hi

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCqkW5R9Ep-GeMGXfM-7tXQ7Tn3MrXWCZM",
+  authDomain: "e-ride-b4fc7.firebaseapp.com",
+  projectId: "e-ride-b4fc7",
+  storageBucket: "e-ride-b4fc7.appspot.com",
+  messagingSenderId: "33590172249",
+  appId: "1:33590172249:web:7084f4b391f04f9d13ef48"
+};
 //
 firebase.initializeApp(firebaseConfig);
 
